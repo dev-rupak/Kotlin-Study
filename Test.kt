f432f237491441f4792e6617ea322ce1177974e0
dev-rupak <118914904+dev-rupak@users.noreply.github.com>
2025-11-06 15:41:52 +0530

    Just Start

---
+++ Test.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val scorelevel1 = 79
+    val scoreLevel2 = 92
+    val scoreLevel3 = 86
+    val totalscore = scorelevel1 + scoreLevel2 + scoreLevel3
+    val averagescore = totalscore / 3
+    println(totalscore)
+    println(averagescore)
+}
