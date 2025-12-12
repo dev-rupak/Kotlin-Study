ba3360d5f796244a4a92be57aeaa494e91c3ec64
dev-rupak <118914904+dev-rupak@users.noreply.github.com>
2025-12-12 14:11:10 +0530

    add Bank Account File
    Signed-off-by: dev-rupak <118914904+dev-rupak@users.noreply.github.com>

---
+++ BankAccount.kt
@@ -0,0 +1,80 @@
+package  Lessons.BankAccount
+
+
+class User(val name: String, val id: Int) {
+    var money: Int = 0
+
+    fun checkBalance() {
+        println("You have $$money in your account")
+    }
+
+    fun deposit() {
+        print("How much do you want to Deposit: ")
+        try {
+            val amount = readln().toInt()
+            if (amount > 0) {
+                money += amount
+                println("You have $$money in your account now")
+            } else {
+                println("Amount must be positive.")
+            }
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Please enter a number.")
+        }
+    }
+
+
+    fun withdraw() {
+        print("How much do you want to Withdraw: ")
+        try {
+            val amount = readln().toInt()
+            if (amount <= money) {
+                money -= amount
+                println("You have $$money in your account now")
+            } else {
+                println("Insufficient Balance!! You only have $$money.")
+            }
+        } catch (e: NumberFormatException) {
+            println("Invalid input. Please enter a number.")
+        }
+    }
+
+    fun showMenu() {
+        println("\nWelcome to your banking system, $name!!")
+
+        var isRunning = true
+
+        while (isRunning) {
+            println("\nWhat type of feature would you like to access?")
+            println("1. Check Balance")
+            println("2. Deposit Money")
+            println("3. Withdraw Money")
+            println("4. Exit")
+            print("Choose option (1-4): ")
+
+            try {
+                val option = readln().toInt()
+                println("Your selected option: $option")
+
+                when (option) {
+                    1 -> checkBalance()
+                    2 -> deposit()
+                    3 -> withdraw()
+                    4 -> {
+                        println("Thank you for banking with us!")
+                        isRunning = false
+                    }
+                    else -> println("Wrong option Chosen. Please try again.")
+                }
+            } catch (e: NumberFormatException) {
+                println("Please enter a valid number.")
+            }
+        }
+    }
+}
+
+fun main() {
+    val user1 = User("Rupak", 100)
+    user1.showMenu()
+}
+
